84ac1ac5d7cef82f68fef9e67de5cb0a5c9e3925
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-11-25 22:22:46 +0100

    A function for calculating the sum of two numbers or concatenating two strings

--- script.ts
+++ script.ts
@@ -1,52 +1,12 @@
-{
-  enum Roles {
-    Admin,
-    Moderator,
-    User,
+function join(valOne: number | string, valTwo: number | string) {
+  if (typeof valOne === "number" && typeof valTwo === "number") {
+    return valOne + valTwo;
+  } else if (typeof valOne === "string" && typeof valTwo === "string") {
+    return valOne.toString() + valTwo.toString();
   }
-
-  const JohnDoe: {
-    id: string | number;
-    firstName: string;
-    lastName: string;
-    role: Roles;
-  }
-  = {
-    id: 1,
-    firstName: 'John',
-    lastName: 'Doe',
-    role: Roles.Admin
-  }
-
-
-  const AmandaDoe: {
-    id: string | number;
-    firstName: string;
-    lastName: string;
-    role: Roles;
-  }
-  = {
-    id: 2,
-    firstName: 'John',
-    lastName: 'Doe',
-    role: Roles.Moderator
-  }
-
-
-  const ThomasJefferson: {
-    id: string | number;
-    firstName: string;
-    lastName: string;
-    role: Roles;
-  }
-  = {
-    id: 'rwe5345sfst3453543',
-    firstName: 'John',
-    lastName: 'Doe',
-    role: Roles.User
-  }
-
-
-  const names: [string, string, string] = [JohnDoe.firstName, AmandaDoe.firstName, ThomasJefferson.firstName]
-  const JohnDoeSummary: [string, string, Roles] = [JohnDoe.firstName, JohnDoe.lastName, JohnDoe.role]
+  return false;
 }
+
+const one = join("John", "Doe"); // should return JohnDoe
+const two = join(5, 6); // should return 11
+const three = join(5, "5"); // should return false
