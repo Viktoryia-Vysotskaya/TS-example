4327b54782bbd1490f6d52c494a82bd9a5e59852
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-11-25 22:29:31 +0100

    Declare the expected values

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function join(valOne, valTwo) {
-    if (typeof valOne === "number" && typeof valTwo === "number") {
-        return valOne + valTwo;
-    }
-    else if (typeof valOne === "string" && typeof valTwo === "string") {
-        return valOne.toString() + valTwo.toString();
-    }
-    return false;
+var permissions = {
+    admin: "rwx",
+    moderator: "rw",
+    user: "r",
+};
+function renderRoleDesc(permissions) {
+    console.log("Role description: ".concat(permissions));
 }
-var one = join("John", "Doe"); // should return JohnDoe
-var two = join(5, 6); // should return 11
-var three = join(5, "5"); // should return false
+function checkRolePermissions(role) {
+    renderRoleDesc(permissions[role]);
+}
--- script.ts
+++ script.ts
@@ -1,12 +1,13 @@
-function join(valOne: number | string, valTwo: number | string) {
-  if (typeof valOne === "number" && typeof valTwo === "number") {
-    return valOne + valTwo;
-  } else if (typeof valOne === "string" && typeof valTwo === "string") {
-    return valOne.toString() + valTwo.toString();
-  }
-  return false;
+const permissions = {
+  admin: "rwx",
+  moderator: "rw",
+  user: "r",
+};
+
+function renderRoleDesc(permissions: string) {
+  console.log(`Role description: ${permissions}`);
 }
 
-const one = join("John", "Doe"); // should return JohnDoe
-const two = join(5, 6); // should return 11
-const three = join(5, "5"); // should return false
+function checkRolePermissions(role: "admin" | "moderator" | "user") {
+  renderRoleDesc(permissions[role]);
+}
