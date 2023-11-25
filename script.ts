61568ee4bf830b5496e5495ae759cdc42702e7bf
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-11-25 22:18:15 +0100

    Add 'enum'

--- script.ts
+++ script.ts
@@ -1,24 +1,52 @@
 {
-  const persons: string[] = []
-
-  function addPerson(firstName: string, lastName: string) {
-    const name: string = firstName + lastName;
-    persons.push(name);
+  enum Roles {
+    Admin,
+    Moderator,
+    User,
   }
 
-  let firstName: string = 'John';
-  let lastName: string = 'Doe';
-  let age: number = 18;
-
-  const person: {
+  const JohnDoe: {
+    id: string | number;
     firstName: string;
-    lastName: string
-  } = {
-    firstName,
-    lastName
+    lastName: string;
+    role: Roles;
   }
-  addPerson(person.firstName, person.lastName);
-  addPerson('Amanda', 'Doe');
-  addPerson('Thomas', 'Jefferson');
+  = {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    role: Roles.Admin
+  }
 
+
+  const AmandaDoe: {
+    id: string | number;
+    firstName: string;
+    lastName: string;
+    role: Roles;
+  }
+  = {
+    id: 2,
+    firstName: 'John',
+    lastName: 'Doe',
+    role: Roles.Moderator
+  }
+
+
+  const ThomasJefferson: {
+    id: string | number;
+    firstName: string;
+    lastName: string;
+    role: Roles;
+  }
+  = {
+    id: 'rwe5345sfst3453543',
+    firstName: 'John',
+    lastName: 'Doe',
+    role: Roles.User
+  }
+
+
+  const names: [string, string, string] = [JohnDoe.firstName, AmandaDoe.firstName, ThomasJefferson.firstName]
+  const JohnDoeSummary: [string, string, Roles] = [JohnDoe.firstName, JohnDoe.lastName, JohnDoe.role]
 }
